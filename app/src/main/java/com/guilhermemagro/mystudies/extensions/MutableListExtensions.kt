6466e08d37963192bd743903a7f6041f5ea040d7
Guilherme Magro <guilherme.magro@hotmail.com>
2022-11-23 16:01:30 -0300

    Improve collapse action

--- app/src/main/java/com/guilhermemagro/mystudies/extensions/MutableListExtensions.kt
+++ app/src/main/java/com/guilhermemagro/mystudies/extensions/MutableListExtensions.kt
@@ -1,9 +1,9 @@
 package com.guilhermemagro.mystudies.extensions
 
-fun <T> MutableList<T>.addOrRemoveIfExist(item: T) {
-    if (contains(item)) {
-        remove(item)
+fun MutableList<String>.addOrRemoveIfExist(string: String) {
+    if (contains(string)) {
+        removeAll { element -> element.startsWith(string) }
     } else {
-        add(item)
+        add(string)
     }
 }
